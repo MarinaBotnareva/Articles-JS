7bb58ac205446789f36bc61245c4489f02e4760b
MarinaBotnareva <botmarinka@gmail.com>
2022-02-24 16:13:20 +0200

    Innitual files

---
+++ index.js
@@ -0,0 +1,22 @@
+const main = document.querySelector('#news');
+
+const news = [
+  { src: './img1.jpeg', title: 'Отличная погода!', text: 'Синоптики обещают отличную погоду в течение всего сезона', button: 'Подробнее'},
+  { src: './img1.jpeg', title: 'Отличная погода!', text: 'Синоптики обещают отличную погоду в течение всего сезона', button: 'Подробнее'},
+  { src: './img1.jpeg', title: 'Отличная погода!', text: 'Синоптики обещают отличную погоду в течение всего сезона', button: 'Подробнее'},
+]
+
+news.forEach((value) => {
+  let article = document.createElement('article');
+  let img = document.createElement('img');
+  img.src = `${value.src}`;
+  let h3 = document.createElement('h3');
+  h3.innerHTML = value.title;
+  let p = document.createElement('p');
+  p.innerHTML = value.text;
+  let button = document.createElement('button');
+  button.innerHTML = value.button;
+
+  article.append(img, h3, p, button);
+  main.append(article);
+})
