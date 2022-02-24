e0e43d4890757d2a02a6eaa64ad6c5a665298f84
MarinaBotnareva <botmarinka@gmail.com>
2022-02-24 16:46:32 +0200

    Add styles

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const main = document.querySelector('#news');
 
 const news = [
-  { src: './img1.jpeg', title: 'Отличная погода!', text: 'Синоптики обещают отличную погоду в течение всего сезона', button: 'Подробнее'},
-  { src: './img1.jpeg', title: 'Отличная погода!', text: 'Синоптики обещают отличную погоду в течение всего сезона', button: 'Подробнее'},
-  { src: './img1.jpeg', title: 'Отличная погода!', text: 'Синоптики обещают отличную погоду в течение всего сезона', button: 'Подробнее'},
+  {src: './img1.jpeg', title: 'Отличная погода!', text: 'Синоптики обещают отличную погоду в течение всего сезона', button: 'Подробнее'},
+  {src: './img1.jpeg', title: 'Отличная погода!', text: 'Синоптики обещают отличную погоду в течение всего сезона', button: 'Подробнее'},
+  {src: './img1.jpeg', title: 'Отличная погода!', text: 'Синоптики обещают отличную погоду в течение всего сезона', button: 'Подробнее'},
 ]
 
 news.forEach((value) => {
   let article = document.createElement('article');
+  article.classList.add("new");
   let img = document.createElement('img');
   img.src = `${value.src}`;
   let h3 = document.createElement('h3');
